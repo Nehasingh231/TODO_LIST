3683b5b4721b11581aadfd02aa71cb3379e5cf41
Nehasingh231 <nehakumari14072001@gmail.com>
2024-07-12 13:25:17 +0530

    aap.js

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import { useState } from "react";
 import TodoList from "./components/TodoList";
 
 
-
 const App = () => {
    const [title, setTitle] = useState("");
    const [mainTask, setMainTask] = useState([]);
+   
    const submitHandler = (e) => {
        e.preventDefault()
        setMainTask([...mainTask, {title}]);
        setTitle("")
    }
+    const deleteHandler = (i) =>{
+       let copyTask = [...mainTask]
+       copyTask.splice(i)
+       setMainTask(copyTask)
+    }
     let renderTask = <h2>No task Available</h2>
     if(mainTask.length > 0) {
-    renderTask = mainTask.map((t, i) => {
+    renderTask = mainTask.map((task, i) => {
       return (
-        <li className="style_task" >
+        <li key={i} className="style_task" >
         <div >
-          <h5>{t.title}</h5>
+          <h5>{task.title}</h5>
         </div>
-        <button >del</button>
+        <span>
+        <i onClick={() => {
+          deleteHandler(i)
+        }} 
+        class="fa-solid fa-trash-can"></i>
+        </span>
         </li>
       );
     });
    }
   return (
    <>
-     <h1>My TODO List</h1>
+     <h1>Things To Do</h1>
 
     <form onSubmit={submitHandler}>
       <input type="text" 
-      placeholder="Enter the Task here"
+      placeholder="What is the task today ?"
       value={title}
       onChange={(e) => {
         setTitle(e.target.value)
